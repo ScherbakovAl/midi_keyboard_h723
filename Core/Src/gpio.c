406776dec46427faa81ec43761d6180c23e8df7d
Scherbakov.al <scherbakov.al@gmail.com>
2025-06-27 23:02:18 +0300

    gpio A4 A5 (relay) -> Open Drain !!

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -68,7 +68,7 @@ void MX_GPIO_Init(void)
   HAL_GPIO_Init(LED_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PAPin PAPin PAPin PAPin */
-  GPIO_InitStruct.Pin = SH_LD_Pin|CLK_Pin|AND_Pin|AND_OFF_Pin;//|0x200|0x8000;//for test (0x200|0x8000);
+  GPIO_InitStruct.Pin = SH_LD_Pin|CLK_Pin|AND_Pin|AND_OFF_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_PULLDOWN;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
@@ -76,8 +76,8 @@ void MX_GPIO_Init(void)
 
   /*Configure GPIO pins : PAPin PAPin */
   GPIO_InitStruct.Pin = POWER_1_Pin|POWER_2_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
